c1db71d0126b03fe7240b7ea935834b661b7ae54
Lars Buitinck <larsmans@gmail.com>
2013-10-31 01:05:06 +0100

    don't pollute the global namespace

--- levenshtein_types.h
+++ levenshtein_types.h
@@ -2,13 +2,13 @@
 
 #include "levenshtein_impl.h"
 
-int levenshtein_char(char const *a, size_t m, char const *b, size_t n)
+static int levenshtein_char(char const *a, size_t m, char const *b, size_t n)
 {
     return levenshtein(a, m, b, n);
 }
 
-int levenshtein_Py_UNICODE(Py_UNICODE const *a, size_t m,
-                           Py_UNICODE const *b, size_t n)
+static int levenshtein_Py_UNICODE(Py_UNICODE const *a, size_t m,
+                                  Py_UNICODE const *b, size_t n)
 {
     return levenshtein(a, m, b, n);
 }
