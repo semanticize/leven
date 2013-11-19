75fc0e0da57f3d7e9ade2d2b6cd63b2d231708c9
Lars Buitinck <l.j.buitinck@esciencecenter.nl>
2013-11-19 18:49:30 +0100

    ship headers in tarball

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup, Extension
 
 setup(
     name='leven',
-    version='1.0.2',
+    version='1.0.4',
     description='Levenshtein edit distance library',
     maintainer='Lars Buitinck',
     maintainer_email='l.j.buitinck@esciencecenter.nl',
     packages=['leven'],
     ext_modules=[
-        Extension("leven._levensthein", ["leven/_levenshtein.cpp"])
+        Extension("leven._levensthein", ["leven/_levenshtein.cpp"],
+                  include_dirs=["leven"])
     ],
     install_requires=["six", "nose"],
     url='https://github.com/semanticize/leven',
