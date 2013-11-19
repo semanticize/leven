c2a3a8c3bc175a8f027331e0032ec600b44f8c8e
Lars Buitinck <l.j.buitinck@esciencecenter.nl>
2013-11-19 18:13:43 +0100

    ship Cython-generated C++ file to facilitate installation
    ... especially in a virtualenv.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.core import setup, Extension
 
 setup(
     name='leven',
-    version='1.0.1',
+    version='1.0.2',
     description='Levenshtein edit distance library',
     maintainer='Lars Buitinck',
     maintainer_email='l.j.buitinck@esciencecenter.nl',
     packages=['leven'],
-    ext_modules=cythonize("leven/_levenshtein.pyx", language="c++"),
+    ext_modules=[
+        Extension("leven._levensthein", ["leven/_levenshtein.cpp"])
+    ],
     install_requires=["six", "nose"],
     url='https://github.com/semanticize/leven',
 )
